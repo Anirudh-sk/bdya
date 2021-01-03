c641e16d8941e90671c97635c095e387b267766c
ANI <47685134+Anirudh-sk@users.noreply.github.com>
2021-01-03 23:09:41 +0530

    Add files via upload

---
+++ js/security.js
@@ -0,0 +1,14 @@
+document.getElementById("pass-input").addEventListener('keydown', (e) => {
+    // console.log(e.key);
+    if (e.key === 'Enter') {
+        const password = (e.currentTarget.value).trim();
+        if (password === "shreeshu05") {
+            // document.getElementById("main").hidden = false;
+            // document.getElementById("pass-input").hidden = true;
+            document.body.innerHTML = `
+            <iframe src="/test.html" frameborder="0" style="width:100%;height:97vh"></iframe>
+            
+            `
+        }
+    }
+})
